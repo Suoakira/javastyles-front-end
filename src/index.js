45e41b9fb8d9dcb7cb3f152421f7b1b5fcf77097
github username <github email address>
2018-11-13 13:50:17 +0000

    can render items on the page

--- src/index.js
+++ src/index.js
@@ -1,21 +1,37 @@
-const listItem = document.querySelector("list-items")
+const showItem = document.querySelector(".show-page")
+const listItem = document.querySelector("#list-items")
 
 
 const renderTop = (top) => {
     let topDiv = document.createElement("div")
     topDiv.className = "top-div"
     topDiv.innerHTML = `
+
+    <img src=${top.frontUrl1} >
     
     `
-    listItem.appendChild("listItem")
+    showItem.appendChild(topDiv)
 
 }
 
 const renderBottom = (bottom) => {
     let bottomDiv = document.createElement("div")
     bottomDiv.innerHTML = `
+
+        <img src=${bottom.frontUrl1} >
     
     `
-    listItem.appendChild("bottomDiv")
+    showItem.appendChild(bottomDiv)
+
+}
+
+const renderShoe = (shoe) => {
+    let shoeDiv = document.createElement("div")
+    shoeDiv.innerHTML = `
+
+        <img src=${shoe.shoe1} >
+    
+    `
+    showItem.appendChild(shoeDiv)
 
 }
--- src/styles.js
+++ src/styles.js
@@ -1,34 +1,34 @@
 
 // demo data we will add backs later
-top = [
+tops = [
 {
-    topFrontUrl1: "https://lp2.hm.com/hmgoepprod?set=source[/8e/be/8ebecfa7270cb9f8f97027c98caa3b2863705470.jpg],origin[dam],category[men_jacketscoats_jackets],type[LOOKBOOK],res[z],hmver[1]&call=url[file:/product/main]",
-    topBackUrl1: "placeholder"
+    frontUrl1: "https://lp2.hm.com/hmgoepprod?set=source[/43/6a/436a2cbc4f0d0bd0b62091f07c5c98dffc1eb1dc.jpg],origin[dam],category[men_jacketscoats_jackets],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl1: "placeholder"
 },
     {
-    topFrontUrl2: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    topBackUrl2: "placeholder"
+    frontUrl2: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl2: "placeholder"
 },
 {
-    topFrontUrl3: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    topBackUrl3: "placeholder"
+    frontUrl3: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl3: "placeholder"
 
 }]
 
-botom = [
+bottoms = [
 {
-    topFrontUrl1: "https://lp2.hm.com/hmgoepprod?set=source[/b9/e3/b9e3483d4e5037cb3dd97e40feb05a29163e32ad.jpg],origin[dam],category[men_trousers_trousers_slim_all],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    topFrontUrl1: "placerholder"
+    frontUrl1: "https://lp2.hm.com/hmgoepprod?set=source[/b9/e3/b9e3483d4e5037cb3dd97e40feb05a29163e32ad.jpg],origin[dam],category[men_trousers_trousers_slim_all],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl1: "placeholder"
 
 }, 
 {
-    topFrontUrl2: "https://lp2.hm.com/hmgoepprod?set=source[/c1/a6/c1a69dc3ac5fd3c3efa89279c179f37710f3f3a4.jpg],origin[dam],category[men_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    topFrontUrl2: "placerholder"
+    frontUrl2: "https://lp2.hm.com/hmgoepprod?set=source[/c1/a6/c1a69dc3ac5fd3c3efa89279c179f37710f3f3a4.jpg],origin[dam],category[men_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl2: "placeholder"
 
 }, 
 {
-    topFrontUrl3: "https://lp2.hm.com/hmgoepprod?set=source[/5c/57/5c576a020bf704706a6ebd1aa2a8ceb7fc050536.jpg],origin[dam],category[men_blazerssuits_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    topFrontUrl3: "placerholder"
+    frontUrl3: "https://lp2.hm.com/hmgoepprod?set=source[/5c/57/5c576a020bf704706a6ebd1aa2a8ceb7fc050536.jpg],origin[dam],category[men_blazerssuits_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl3: "placeholder"
 
 }]
 
