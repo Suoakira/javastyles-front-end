18dbedc3fc25fb51aefac807f06d096dfbb077d6
github username <github email address>
2018-11-13 14:48:14 +0000

    a user can scroll through the items

--- src/index.js
+++ src/index.js
@@ -1,21 +1,88 @@
+const tops = [
+{
+    frontUrl: "https://lp2.hm.com/hmgoepprod?set=source[/43/6a/436a2cbc4f0d0bd0b62091f07c5c98dffc1eb1dc.jpg],origin[dam],category[men_jacketscoats_jackets],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl: "placeholder"
+},
+    {
+    frontUrl: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl: "placeholder"
+},
+{
+    frontUrl: "https://lp2.hm.com/hmgoepprod?set=source[/52/09/5209c7bf06008391ad811fd088fa548baa658e36.jpg],origin[dam],category[men_tshirtstanks_shortsleeve],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+    backUrl: "placeholder"
+
+}]
+
+
+
+
+
+
+
+
 const showItem = document.querySelector(".show-page")
 const listItem = document.querySelector("#list-items")
+let topCounter = 0
+let bottomCounter = 0
+let shoeCounter = 0
 
 
-const renderTop = (top) => {
+// for click of right arrow button
+const goRight = () => {
+    if (topCounter !== tops.length - 1){
+        topCounter++
+    } else {
+        topCounter = 0 
+    }
+}
+
+// for click of left arrow button
+const goLeft = () => {
+    if (topCounter === 0){
+        topCounter = tops.length-1
+    } else {
+        --topCounter
+    }
+}
+
+
+
+const renderTop = () => {
     let topDiv = document.createElement("div")
     topDiv.className = "top-div"
     topDiv.innerHTML = `
 
-    <img src=${top.frontUrl1} >
-    
+    <img src=${tops[topCounter].frontUrl} />
+    <button class="right-top">RIGHT</button>
+    <button class="left-top">LEFT</button>
     `
     showItem.appendChild(topDiv)
+    
+    const rightButton = document.querySelector('.right-top')
+    const leftButton = document.querySelector('.left-top')
+
+    rightButton.addEventListener('click', () => {
+        goRight()
+        showItem.innerHTML = ""
+        renderTop(tops)
+    })
+
+    leftButton.addEventListener('click', () => {
+        goLeft()
+        showItem.innerHTML = ""
+        renderTop(tops)
+    })
 
 }
 
+renderTop(tops)
+
+
+
+
 const renderBottom = (bottom) => {
     let bottomDiv = document.createElement("div")
+    bottomDiv.className = "bottom-div"
     bottomDiv.innerHTML = `
 
         <img src=${bottom.frontUrl1} >
@@ -35,3 +102,4 @@ const renderShoe = (shoe) => {
     showItem.appendChild(shoeDiv)
 
 }
+
--- src/styles.js
+++ src/styles.js
@@ -1,44 +1,44 @@
 
-// demo data we will add backs later
-tops = [
-{
-    frontUrl1: "https://lp2.hm.com/hmgoepprod?set=source[/43/6a/436a2cbc4f0d0bd0b62091f07c5c98dffc1eb1dc.jpg],origin[dam],category[men_jacketscoats_jackets],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    backUrl1: "placeholder"
-},
-    {
-    frontUrl2: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    backUrl2: "placeholder"
-},
-{
-    frontUrl3: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    backUrl3: "placeholder"
+// // demo data we will add backs later
+// const tops = [
+// {
+//     frontUrl: "https://lp2.hm.com/hmgoepprod?set=source[/43/6a/436a2cbc4f0d0bd0b62091f07c5c98dffc1eb1dc.jpg],origin[dam],category[men_jacketscoats_jackets],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+//     backUrl: "placeholder"
+// },
+//     {
+//     frontUrl: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+//     backUrl: "placeholder"
+// },
+// {
+//     frontUrl: "https://lp2.hm.com/hmgoepprod?set=source[/9f/b6/9fb6a93bd313731bd807dfdd5a79d26665b79707.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+//     backUrl: "placeholder"
 
-}]
+// }]
 
-bottoms = [
-{
-    frontUrl1: "https://lp2.hm.com/hmgoepprod?set=source[/b9/e3/b9e3483d4e5037cb3dd97e40feb05a29163e32ad.jpg],origin[dam],category[men_trousers_trousers_slim_all],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    backUrl1: "placeholder"
+// const bottoms = [
+// {
+//     frontUrl1: "https://lp2.hm.com/hmgoepprod?set=source[/b9/e3/b9e3483d4e5037cb3dd97e40feb05a29163e32ad.jpg],origin[dam],category[men_trousers_trousers_slim_all],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+//     backUrl1: "placeholder"
 
-}, 
-{
-    frontUrl2: "https://lp2.hm.com/hmgoepprod?set=source[/c1/a6/c1a69dc3ac5fd3c3efa89279c179f37710f3f3a4.jpg],origin[dam],category[men_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    backUrl2: "placeholder"
+// }, 
+// {
+//     frontUrl2: "https://lp2.hm.com/hmgoepprod?set=source[/c1/a6/c1a69dc3ac5fd3c3efa89279c179f37710f3f3a4.jpg],origin[dam],category[men_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+//     backUrl2: "placeholder"
 
-}, 
-{
-    frontUrl3: "https://lp2.hm.com/hmgoepprod?set=source[/5c/57/5c576a020bf704706a6ebd1aa2a8ceb7fc050536.jpg],origin[dam],category[men_blazerssuits_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
-    backUrl3: "placeholder"
+// }, 
+// {
+//     frontUrl3: "https://lp2.hm.com/hmgoepprod?set=source[/5c/57/5c576a020bf704706a6ebd1aa2a8ceb7fc050536.jpg],origin[dam],category[men_blazerssuits_trousers],type[DESCRIPTIVESTILLLIFE],res[z],hmver[1]&call=url[file:/product/main]",
+//     backUrl3: "placeholder"
 
-}]
+// }]
 
-shoes = [
-{
-    shoe1: "https://runnerspoint.scene7.com/is/image/rpe/314625664504?$medium$"
-},
-{
-    shoe2: "https://runnerspoint.scene7.com/is/image/rpe/314625649604?$medium$"
-},
-{
-    shoe3: "https://runnerspoint.scene7.com/is/image/rpe/314625688404?$medium$"
-}]
+// const shoes = [
+// {
+//     shoe1: "https://runnerspoint.scene7.com/is/image/rpe/314625664504?$medium$"
+// },
+// {
+//     shoe2: "https://runnerspoint.scene7.com/is/image/rpe/314625649604?$medium$"
+// },
+// {
+//     shoe3: "https://runnerspoint.scene7.com/is/image/rpe/314625688404?$medium$"
+// }]
